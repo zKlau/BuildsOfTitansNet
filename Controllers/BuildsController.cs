aa688d0a21d0c39f23a18ae13aad092acbd2a880
zKlau <37957524+zKlau@users.noreply.github.com>
2026-01-01 17:49:42 +0200

    Add build and ownership endpoint

--- Controllers/BuildsController.cs
+++ Controllers/BuildsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using BuildsOfTitansNet.Data;
 using Microsoft.EntityFrameworkCore;
 using BuildsOfTitansNet.Services;
+using BuildsOfTitansNet.Models;
 
 [ApiController]
 [Route("v1/[controller]")]
@@ -16,17 +17,68 @@ public class BuildsController : ControllerBase
         _dbContext = dbContext;
         _currentUserService = currentUserService;
     }
-
-    private async Task<object> ReturnBuildPreview(BuildsOfTitansNet.Models.Build s, BuildsOfTitansNet.Models.User? currentUser)
+    private async Task<object> ReturnBuild(BuildsOfTitansNet.Models.Build s, BuildsOfTitansNet.Models.User? currentUser)
     {
-
-
         return new
         {
             s.Id,
             s.Name,
             created_at = s.CreatedAt,
-            user_name = s.User.Name,
+            user_name = s.User?.Name ?? "Unknown",
+            dinosaur = new
+            {
+                s.Species.Id,
+                s.Species.Name,
+                s.Species.Icon,
+                s.Species.Description,
+                diet = new { s.Species.Diet.Id, s.Species.Diet.Name },
+                base_stat = new
+                {
+                    s.Species.BaseStat?.Damage,
+                    s.Species.BaseStat?.Defense,
+                    s.Species.BaseStat?.Recovery,
+                    s.Species.BaseStat?.LandSpeed,
+                    s.Species.BaseStat?.WaterSpeed,
+                    s.Species.BaseStat?.Survivability
+                },
+                slots = s.Species.SpeciesAbilitySlots.Select(sas => new
+                {
+                    id = sas.AbilitySlot.Id,
+                    name = sas.AbilitySlot.Name,
+                    limit = sas.Limit
+                }),
+                subspecies = s.Subspecies
+            },
+            votes = new
+            {
+                user_vote = s.BuildVotes.FirstOrDefault(v => v.UserId == currentUser?.Id)?.VoteType ?? null,
+                upvotes = s.BuildVotes.Count(v => v.VoteType == 0),
+                downvotes = s.BuildVotes.Count(v => v.VoteType == 1)
+            },
+            abilities = s.BuildAbilities.Select(ba => new
+            {
+                id = ba.Id,
+                slots = new
+                {
+                    id = ba.DinosaurAbility.AbilitySlot.Id,
+                    name = ba.DinosaurAbility.AbilitySlot.Name
+                },
+                name = ba.DinosaurAbility.Ability.Name,
+                description = ba.DinosaurAbility.Description,
+                icon = ba.DinosaurAbility.Ability.Icon,
+                price = ba.DinosaurAbility.Price,
+            })
+        };
+    }
+
+    private async Task<object> ReturnBuildPreview(BuildsOfTitansNet.Models.Build s, BuildsOfTitansNet.Models.User? currentUser)
+    {
+        return new
+        {
+            s.Id,
+            s.Name,
+            created_at = s.CreatedAt,
+            user_name = s.User?.Name ?? "Unknown",
             dinosaur = new
             {
                 s.Species.Id,
@@ -50,9 +102,10 @@ public class BuildsController : ControllerBase
 
         var builds = await _dbContext.Builds
             .Where(b => b.Species.Name.ToLower() == name.ToLower())
+            .Include(b => b.User)
+            .Where(b => b.User != null)
             .Include(b => b.Species)
             .Include(b => b.Subspecies)
-            .Include(b => b.User)
             .Include(b => b.BuildVotes)
             .OrderByDescending(b => b.BuildVotes.Count(v => v.VoteType == 0))
             .ToListAsync();
@@ -61,5 +114,58 @@ public class BuildsController : ControllerBase
 
         return Ok(new { builds = buildPreviews });
     }
+
+    [HttpGet("owner/{id}")]
+    public async Task<IActionResult> GetBuildIsOwner(int id)
+    {
+        var currentUser = await _currentUserService.GetCurrentUserAsync();
+
+        var build = await _dbContext.Builds
+            .Include(b => b.User)
+            .Where(b => b.Id == id)
+            .FirstOrDefaultAsync();
+
+        return Ok(new { isOwner = build != null && currentUser != null && build.UserId == currentUser.Id });
+
+    }
+
+    [HttpGet("id/{id}")]
+    public async Task<IActionResult> GetBuild(int id)
+    {
+        var currentUser = await _currentUserService.GetCurrentUserAsync();
+
+        var build = await _dbContext.Builds
+            .Include(b => b.User)
+            .Include(b => b.Species)
+                .ThenInclude(s => s.Diet)
+            .Include(b => b.Species)
+                .ThenInclude(s => s.BaseStat)
+            .Include(b => b.Species)
+                .ThenInclude(s => s.SpeciesAbilitySlots)
+                    .ThenInclude(sas => sas.AbilitySlot)
+            .Include(b => b.Subspecies)
+            .Include(b => b.BuildVotes)
+            .Include(b => b.BuildAbilities)
+                .ThenInclude(ba => ba.DinosaurAbility)
+                    .ThenInclude(da => da.Ability)
+            .Include(b => b.BuildAbilities)
+                .ThenInclude(ba => ba.DinosaurAbility)
+                    .ThenInclude(da => da.AbilitySlot)
+            .Where(b => b.Id == id)
+            .FirstOrDefaultAsync();
+
+        if (build == null || build.User == null || build.Species == null || build.Subspecies == null ||
+            build.BuildAbilities.Any(ba => ba.DinosaurAbility == null ||
+                                          ba.DinosaurAbility.Ability == null ||
+                                          ba.DinosaurAbility.AbilitySlot == null))
+        {
+            return NotFound(new { message = "Build not found" });
+        }
+
+        var result = await ReturnBuild(build, currentUser);
+
+        return Ok(result);
+    }
+
 
 }
